b239518c23423fc6167bad7fe4b9986152901c3c
Parvathyanoop <parvathyanoop2015@gmail.com>
2016-10-10 14:03:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Content={
+    title:'Article_ONE|Parvathy Komalan S',
+    heading:'Article_ONE',
+    date:'10/10/2016',
+    content :`
+    <p>
+            This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This \ article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.
+            </p>
+        
+            <p>
+            This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.
+        </p>
+        <p>
+            This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.
+        </p>
+        <p>
+            This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.This is article one.
+        </p>`
+    
+    
+};
+
+function createtemplate (data){
+    title=data.title;
+    heading=data.heading;
+    date=data.date;
+    content=data.content;
+var htmltemplate =`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class ='container'>
+    <div>
+        <a href="/">
+           Back 
+        </a>
+    </div>
+    <hr/>
+    <h4>
+        ${heading}
+    </h4>
+    <div>
+       ${date}
+    </div>
+    <div>
+        ${content}
+    </div>
+    </div>
+   </body> 
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
+  res.send(createtemplate(articleone));
 });
 app.get('/article_two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
